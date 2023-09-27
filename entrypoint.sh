5ddcdd8574baa685a6627b6c8c65168cfbd45725
bcarranza <bayron.carranza@gmail.com>
2023-09-27 15:49:16 -0600

    tg plan

--- entrypoint.sh
+++ entrypoint.sh
@@ -106,9 +106,9 @@ run_accurics() {
   echo "------------------"
   # Run accurics plan
   #accurics $INPUT_RUN_MODE $params $plan_args $pipeline_mode
-  chmod +x /usr/bin/accurics
   
-  accurics $INPUT_RUN_MODE $params $plan_args $pipeline_mode
+  accurics tgplan
+  #accurics $INPUT_RUN_MODE $params $plan_args $pipeline_mode
   
   ACCURICS_PLAN_ERR=$?
 }
